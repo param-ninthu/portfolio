20bb05927733412de02b2560c09d6708ce53a24a
param-ninthu <ninthu1999@gmail.com>
2022-12-29 00:04:37 +0530

    home page

--- src/screens/home/homePage.jsx
+++ src/screens/home/homePage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import {
+  BodyContainer,
+  Logo,
+  NavButtonContainer,
+  NavContainer,
+  NavLinkContainer,
+} from "./homePageElements";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <BodyContainer>
+        <NavContainer>
+          <Logo>choosen</Logo>
+          <NavLinkContainer>Navlinks</NavLinkContainer>
+          <NavButtonContainer>Button</NavButtonContainer>
+        </NavContainer>
+      </BodyContainer>
+    </>
+  );
 };
 
 export default Home;
--- src/screens/home/homePageElements.jsx
+++ src/screens/home/homePageElements.jsx
@@ -1,1 +1,35 @@
 import styled from "styled-components";
+
+export const BodyContainer = styled.div`
+  padding: 0;
+  margin: 0;
+  height: 100vh;
+  background-color: #d9e1e6;
+  width: 100%;
+  overflow: hidden;
+`;
+
+export const NavContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Logo = styled.div`
+  font-weight: 900;
+  font-size: 2rem;
+  margin-left: 5px;
+`;
+
+export const NavLinkContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NavButtonContainer = styled.div`
+  margin-right: 5px;
+`;
