52cc1c18492849772d7edb1f52031da0c105f752
param-ninthu <ninthu1999@gmail.com>
2023-03-19 21:56:11 +0530

    colapse

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,74 +1,9 @@
-import React from "react";
-import {
-  BodyContainer,
-  ResumeLink,
-  List,
-  Logo,
-  NavButtonContainer,
-  NavContainer,
-  NavLinkContainer,
-  UoList,
-} from "../elements/NavBarElements";
+import React from 'react'
 
-import Resume from "./../assets/pdf/NinthuCV.pdf";
-
-import CV from "./../assets/images/CV.png";
-
-const NavBar = () => {
+const Navbar = () => {
   return (
-    <>
-      <Logo>chosen</Logo>
-      <NavLinkContainer>
-        <UoList>
-          <List
-            to="home"
-            smooth={true}
-            duration={500}
-            spy={true}
-            exact="true"
-            offset={-80}
-          >
-            Home
-          </List>
-          <List
-            to="experiences"
-            smooth={true}
-            duration={500}
-            spy={true}
-            exact="true"
-            offset={-80}
-          >
-            Experience
-          </List>
-          <List
-            to="projects"
-            smooth={true}
-            duration={500}
-            spy={true}
-            exact="true"
-            offset={-80}
-          >
-            Projects
-          </List>
-          <List
-            to="contact"
-            smooth={true}
-            duration={500}
-            spy={true}
-            exact="true"
-            offset={-80}
-          >
-            Contact
-          </List>
-          <NavButtonContainer>
-            <ResumeLink href={CV} target={"_blank"}>
-              Resume
-            </ResumeLink>
-          </NavButtonContainer>
-        </UoList>
-      </NavLinkContainer>
-    </>
-  );
-};
+    <div>Navbar</div>
+  )
+}
 
-export default NavBar;
+export default Navbar
