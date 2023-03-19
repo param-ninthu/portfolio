d7135d09f28d7abc8708f1c8653ce5ce993dca21
param-ninthu <ninthu1999@gmail.com>
2023-03-19 15:23:11 +0530

    Ci cd check

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -14,7 +14,7 @@ const Work = () => {
     <>
       <InnnerContainer>
         <Title>
-          Experience <hr style={{ width: "50%" }} />
+          Experienc <hr style={{ width: "50%" }} />
         </Title>
         <Heading> Where I've Worked</Heading>
         <SubHeading>
