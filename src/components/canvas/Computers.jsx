69ddd7c7cd588fb964ff03009249883f4903552b
param-ninthu <ninthu1999@gmail.com>
2023-03-20 22:47:55 +0530

    error on three package

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { Suspense, useState, useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return <div>Computers</div>;
 };
 
