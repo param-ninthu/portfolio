1c3b13dad4857c6178f31f4775b22f4a485bc286
param-ninthu <ninthu1999@gmail.com>
2023-03-21 16:52:47 +0530

    hero updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,6 +22,23 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute bottom-4 w-full flex justify-center items-center mt-10">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 ">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            ></motion.dev>
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
