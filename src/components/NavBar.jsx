db1af876d77800cea9d088d9a2fc6d39db09f0b3
param-ninthu <ninthu1999@gmail.com>
2023-04-05 21:08:37 +0530

    navbar undo changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import { styles } from "./../styles";
 import { navLinks } from "./../constants/index";
 import { logo, menu, close } from "./../assets";
-import { SectionWrapper } from "../hoc";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -75,4 +74,4 @@ const Navbar = () => {
   );
 };
 
-export default SectionWrapper(Navbar, "navbar");
+export default Navbar;
